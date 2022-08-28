32f869c853d401019f78bfd85d0bcd0832b6cf1a
Princess Nkabinde <52870123+Princessn@users.noreply.github.com>
2022-08-28 23:52:42 +0200

    changes

--- namesGreeted.js
+++ namesGreeted.js
@@ -4,6 +4,7 @@ module.exports = (db) => {
     const setName = async (name) => {
         username = name.toLowerCase().trim()
         let counter =  db.manyOrNone("select count(*) from users where name=$1", [username])
+        console.log (counter)
         if (counter == null){
             await db.none("insert into users(name,counter) values($1,1)", [username])
         }
